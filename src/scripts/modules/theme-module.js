e9aeda230bf705bfa11efd33ef6c6c996c9add4a
rosalinda-fullstack <rosalinda@fullstack.ph>
2021-03-17 16:28:21 -0700

    fixed about-page issue

--- src/scripts/modules/theme-module.js
+++ src/scripts/modules/theme-module.js
@@ -6,18 +6,43 @@ AppName.Modules.ThemeModule = (function () {
   // Private Methods //
   ////////////////////
   const _privateMethod = () => {
-    var _swiperMini = new Swiper('.swiper-container');
+    // private stuff
 
-    var _swiperReading = new Swiper('.swiper-reading', {
-      watchSlidesProgress: true,
-      watchSlidesVisibility: true,
-      slidesPerView: 'auto',
-    });
-
-     const _swiperBanner = new Swiper('.swiper-banner', {
-      autoplay: {
+    const swiper_banner = new Swiper('.swiper-banner', {
+     autoplay: {
       delay: 2500,
       disableOnInteraction: false,
+    },
+      pagination: {
+        el: '.swiper-pagination-banner',
+      },
+    });
+
+    var swiper_reading = new Swiper('.swiper-reading', {
+      slidesPerView: 0,
+      spaceBetween: 0,
+      pagination: {
+        el: '.swiper-pagination-reading',
+        clickable: true,
+      },
+      breakpoints:{
+        575:{
+          slidesPerView: 0,
+          spaceBetween: 0,
+        },
+  
+      767:{
+        slidesPerView: 0,
+        spaceBetween: 0,
+      },
+      991:{
+        slidesPerView: 0,
+        spaceBetween: 0,
+      },
+      1199:{
+        slidesPerView: 0,
+        spaceBetween: 0,
+      }
     },
        pagination: {
          el: '.swiper-pagination-banner',
@@ -28,37 +53,30 @@ AppName.Modules.ThemeModule = (function () {
   
     function lockScroll() {
       if ($('body').hasClass('lock-scroll')) {
-          $('body').removeClass('lock-scroll');
+        $('body').removeClass('lock-scroll');
       }
       else {
-          $('body').addClass('lock-scroll');
+        $('body').addClass('lock-scroll');
       }
     }  
     
     $(document).ready(function() {
       $('.icon-bar').click(function() {
-         lockScroll();
+        lockScroll();
       }); 
     });
+  
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 50){  
+        $('header').addClass("sticky");
+      }
+      else {
+        $('header').removeClass("sticky");
+      }
 
-    function initHeader() {
-      var Header = document.querySelector("header");
-    
-      var HeaderContainer = document.querySelector("header");
-    
-      document.addEventListener('scroll', function () {
-            if (window.scrollY > 114) {
-                Header.classList.add('sticky');
-                HeaderContainer.classList.add('sticky');
-            } else {
-                Header.classList.remove('sticky');
-                HeaderContainer.classList.remove('sticky');
-            }
-      });
-    }
-   
-    window.onload = initHeader;
-
+      console.log('scroll')
+    });
+  });
   /////////////////////
   // Public Methods //
   ///////////////////
@@ -70,8 +88,5 @@ AppName.Modules.ThemeModule = (function () {
   return {
     init: init,
   };
-})();
 
-function scrollToTop() {
-  window.scrollTo({top: 0, behavior: 'smooth'});
-  }
+ 
